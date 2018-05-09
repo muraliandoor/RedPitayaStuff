261b53cadad3ca020214f3f4fe8e5fe3c758d19f
wecassidy <wec.notif.github@gmail.com>
2018-05-09 14:32:39 -0400

    Add simple range checks on SP, etc. All are signed 14-bit integers.

--- FPGA_PID.py
+++ FPGA_PID.py
@@ -13,17 +13,29 @@ class FPGA_PID:
         self.integral(k_integral)
 
     def set_point(self, val):
+        if val < -2**13 or val > 2**13 - 1:
+            raise ValueError("Set point out of range.")
+
         self.sp = val
         subprocess.run(["monitor", 0x40300010, val])
 
     def proportional(self, val):
+        if val < -2**13 or val > 2**13 - 1:
+            raise ValueError("Proportional constant out of range.")
+
         self.kp = val
         subprocess.run(["monitor", 0x40300014, val])
 
     def derivative(self, val):
+        if val < -2**13 or val > 2**13 - 1:
+            raise ValueError("Derivative constant out of range.")
+
         self.kd = val
         subprocess.run(["monitor", 0x4030001c, val])
 
     def integral(self, val):
+        if val < -2**13 or val > 2**13 - 1:
+            raise ValueError("Integral constant out of range.")
+
         self.ki = val
         subprocess.run(["monitor", 0x40300018, val])

241f525b658ac4ec3c1dee64f7f5186ef9eb6c2b
wecassidy <wec.notif.github@gmail.com>
2018-05-09 16:01:12 -0400

    Use any of the four PIDs.

--- FPGA_PID.py
+++ FPGA_PID.py
@@ -1,46 +1,48 @@
 import subprocess
 
+SP_BUFFER = {(1, 1): 0x40300010, (1, 2): 0x40300020, (2, 1): 0x40300030, (2, 2): 0x40300040}
+KP_BUFFER = {(1, 1): 0x40300014, (1, 2): 0x40300024, (2, 1): 0x40300034, (2, 2): 0x40300044}
+KD_BUFFER = {(1, 1): 0x4030001c, (1, 2): 0x4030002c, (2, 1): 0x4030003c, (2, 2): 0x4030004c}
+KI_BUFFER = {(1, 1): 0x40300018, (1, 2): 0x40300028, (2, 1): 0x40300038, (2, 2): 0x40300048}
+
 class FPGA_PID:
     """
     Interface with the buffers that control the Red Pitaya's FPGA PID
     implementation.
     """
 
-    SP_BUFFER = 0x40300010
-    KP_BUFFER = 0x40300014
-    KD_BUFFER = 0x4030001c
-    KI_BUFFER = 0x40300018
-
-    def __init__(self, set_point, k_proportional, k_derivative, k_integral):
+    def __init__(self, in_channel, out_channel, set_point, k_proportional, k_derivative, k_integral):
         self.set_point(set_point)
         self.proportional(k_proportional)
         self.derivative(k_derivative)
         self.integral(k_integral)
+
+        self.channels = (in_channel, out_channel)
 
     def set_point(self, val):
         if val < -2**13 or val > 2**13 - 1:
             raise ValueError("Set point out of range.")
 
         self.sp = val
-        subprocess.run(["monitor", FPGA_PID.SP_BUFFER, val])
+        subprocess.run(["monitor", SP_BUFFER[self.channels], val])
 
     def proportional(self, val):
         if val < -2**13 or val > 2**13 - 1:
             raise ValueError("Proportional constant out of range.")
 
         self.kp = val
-        subprocess.run(["monitor", FPGA_PID.KP_BUFFER, val])
+        subprocess.run(["monitor", KP_BUFFER[self.channels], val])
 
     def derivative(self, val):
         if val < -2**13 or val > 2**13 - 1:
             raise ValueError("Derivative constant out of range.")
 
         self.kd = val
-        subprocess.run(["monitor", FPGA_PID.KD_BUFFER, val])
+        subprocess.run(["monitor", KD_BUFFER[self.channels], val])
 
     def integral(self, val):
         if val < -2**13 or val > 2**13 - 1:
             raise ValueError("Integral constant out of range.")
 
         self.ki = val
-        subprocess.run(["monitor", FPGA_PID.KI_BUFFER, val])
+        subprocess.run(["monitor", KI_BUFFER[self.channels], val])

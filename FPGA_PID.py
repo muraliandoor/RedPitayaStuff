edc020f6cadb3077e5e8581b057056e9a0e55428
wecassidy <wec.notif.github@gmail.com>
2018-05-09 14:57:18 -0400

    Move buffer locations to constants.

--- FPGA_PID.py
+++ FPGA_PID.py
@@ -6,6 +6,11 @@ class FPGA_PID:
     implementation.
     """
 
+    SP_BUFFER = 0x40300010
+    KP_BUFFER = 0x40300014
+    KD_BUFFER = 0x4030001c
+    KI_BUFFER = 0x40300018
+
     def __init__(self, set_point, k_proportional, k_derivative, k_integral):
         self.set_point(set_point)
         self.proportional(k_proportional)
@@ -17,25 +22,25 @@ class FPGA_PID:
             raise ValueError("Set point out of range.")
 
         self.sp = val
-        subprocess.run(["monitor", 0x40300010, val])
+        subprocess.run(["monitor", FPGA_PID.SP_BUFFER, val])
 
     def proportional(self, val):
         if val < -2**13 or val > 2**13 - 1:
             raise ValueError("Proportional constant out of range.")
 
         self.kp = val
-        subprocess.run(["monitor", 0x40300014, val])
+        subprocess.run(["monitor", FPGA_PID.KP_BUFFER, val])
 
     def derivative(self, val):
         if val < -2**13 or val > 2**13 - 1:
             raise ValueError("Derivative constant out of range.")
 
         self.kd = val
-        subprocess.run(["monitor", 0x4030001c, val])
+        subprocess.run(["monitor", FPGA_PID.KD_BUFFER, val])
 
     def integral(self, val):
         if val < -2**13 or val > 2**13 - 1:
             raise ValueError("Integral constant out of range.")
 
         self.ki = val
-        subprocess.run(["monitor", 0x40300018, val])
+        subprocess.run(["monitor", FPGA_PID.KI_BUFFER, val])
